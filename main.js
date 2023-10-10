597bacd0c2681aa0bc6ea18fb094c138fdeefa5a
Bexruz <141207743+CrayzeS07@users.noreply.github.com>
2023-10-10 08:57:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//ixtiyoriy son
+let Uzb = 777;
+if (Uzb % 2 === 0) {
+  console.log('juft son');
+}
+ else {
+  console.log('toq son');
+}
+//ixtiyoriy A va B Son
+var matter1 = 571;
+var matter2 = 70; 
+
+console.log("sonlar: " + matter1 + ", " + matter2);
+
+if (Math.abs(matter1 - 5) < Math.abs(matter2 - 5)) {
+  console.log(a + "5 ga yaqinroq");
+} else if (Math.abs(matter1 - 5) > Math.abs(matter2 - 5)) {
+  console.log(matter2 + " soni 5 soniga yaqinroq");
+} else {
+  console.log("Ikkala son ham 5 ga yaqin");
+}
+
